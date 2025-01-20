abd90c019d3fff89e55f156c53c846e6b6fe1232
apella1 <johncollons5@gmail.com>
2025-01-20 16:26:06 +0300

    feat: set up login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import SocialButton from "@/components/ui/SocialButton";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+interface Login {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Login>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/chat");
+  };
+
+  return (
+    <div className="grid h-screen place-items-center bg-login bg-cover">
+      <form
+        className="p-8 rounded-2xl border border-gray-400 shadow flex flex-col space-y-3 bg-gray-100 w-[400px]"
+        onSubmit={handleSubmit(handleLogin)}
+      >
+        <div className="self-center flex flex-col">
+          <h1 className="self-center text-lg font-medium">
+            Sign in with email
+          </h1>
+          <p className="text-sm self-center text-gray-600">
+            Sign in to access your chats
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("email", {
+            required: "Email address is required!",
+          })}
+        />
+        {errors.email && (
+          <p className="text-xs text-red-600">{errors.email.message}</p>
+        )}
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: "Password is required!" })}
+        />
+        {errors.password && (
+          <p className="text-xs text-red-600">{errors.password?.message}</p>
+        )}
+        <p className="self-end text-sm underline underline-offset-2 cursor-pointer">
+          Forgot password?
+        </p>
+        <button className="w-full px-2 py-1.5 text-white text-sm bg-gray-800 rounded-lg font-medium">
+          Get Started
+        </button>
+        <p className="text-xs self-center text-gray-600">Or sign in with</p>
+        <div className="flex items-center space-x-4 self-center">
+          <SocialButton icon={<FcGoogle />} />
+          <SocialButton icon={<FaGithub />} />
+        </div>
+      </form>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        login: "url(/images/login-bg.jpg)",
+      },
     },
   },
   plugins: [],
