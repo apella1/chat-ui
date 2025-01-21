8312396b4bde9423f10d3b6ab5d946ae5191c46b
apella1 <johncollons5@gmail.com>
2025-01-21 16:59:05 +0300

    feat: update app side menu

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,28 +1,41 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { MdOutlineSettings } from "react-icons/md";
 import { RiChatSmile2Line } from "react-icons/ri";
 import { TiMessages } from "react-icons/ti";
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
+  const chatPage = "/chat";
+  const settingsPage = "/chat/settings";
+  const pathname = usePathname();
+
   return (
     <main className="flex h-screen overflow-hidden">
-      <aside className="flex w-full basis-1/12">
-        <div className="flex w-full flex-col bg-gray-800 px-4 py-8">
-          <div className="self-center">
-            <RiChatSmile2Line className="text-4xl text-purple-600" />
-          </div>
-          <div className="flex flex-col space-y-8 self-center pt-32">
-            <Link href={"/chat"} aria-label="Messages">
-              <TiMessages className="text-2xl text-purple-600" />
-            </Link>
-            <Link href={"/chat/settings"} aria-label="Settings">
-              <MdOutlineSettings className="text-2xl text-purple-600" />
-            </Link>
-          </div>
+      <aside className="flex w-fit flex-col bg-gray-800 px-4 py-8">
+        <div className="self-center">
+          <RiChatSmile2Line className="text-4xl text-purple-600" />
+        </div>
+        <div className="flex flex-col space-y-8 self-center pt-32">
+          <Link
+            href={chatPage}
+            aria-label="Messages"
+            className={`${pathname === chatPage ? "" : ""}`}
+          >
+            <TiMessages className="text-2xl text-purple-600" />
+          </Link>
+          <Link
+            href={settingsPage}
+            aria-label="Settings"
+            className={`${pathname === settingsPage ? "" : ""}`}
+          >
+            <MdOutlineSettings className="text-2xl text-purple-600" />
+          </Link>
         </div>
       </aside>
-      <section className="basis-11/4 flex h-screen w-full flex-col overflow-y-auto">
+      <section className="flex h-screen w-full flex-grow flex-col overflow-y-auto">
         {children}
       </section>
     </main>
